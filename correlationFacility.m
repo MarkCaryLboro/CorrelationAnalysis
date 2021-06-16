a28ea83f6afe24b33c1892607fc474f5bf701f76
MarkCaryLboro <64597423+MarkCaryLboro@users.noreply.github.com>
2021-06-16 08:00:01 +0100

    Added enumeration for facility

---
+++ correlationFacility.m
@@ -0,0 +1,9 @@
+classdef correlationFacility < int8
+    enumeration
+        Lancaster   (0)
+        Imperial    (1)
+        Oxford      (2)
+        Warwick     (3)
+        Birmingham  (4)
+    end
+end % correlationFacility
