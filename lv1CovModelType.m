68b5721312eed0b1aa9923840c0ff9c8688f9abe
MarkCaryLboro <64597423+MarkCaryLboro@users.noreply.github.com>
2022-02-08 12:13:31 +0000

    deleted level1 model abstraction

--- lv1CovModelType.m
+++
@@ -1,9 +0,0 @@
-classdef lv1CovModelType < int8
-    % Define supported level-1 model covariance models
-    
-    enumeration
-        OLS     (0)
-        Power   (1)
-        TwoComp (2)
-    end
-end % classdef
